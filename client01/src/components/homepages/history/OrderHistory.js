a181b4288cadfc2f886e8dc50e9307b7447e2b4c
Mkhululi Cebani <mkhululicebani01@gmail.com>
2022-08-14 18:07:26 +0200

    io server change

--- client01/src/components/homepages/history/OrderHistory.js
+++ client01/src/components/homepages/history/OrderHistory.js
@@ -8,6 +8,7 @@ const OrderHistory = () => {
     const [history] = state.userAPI.history
     
     const [display,setDisplay] = useState([])
+    const [sortedDisplay, setSortedDisplay] = useState([])
     const [userId] = state.userAPI.userId
     const [isAdmin] = state.userAPI.isAdmin
     
@@ -31,10 +32,24 @@ const OrderHistory = () => {
             }
         }
     },[history, isAdmin,userId])
+
+
+    useEffect(() => {
+      
+        if(display.length > 1)
+        {
+            let sorted = display.sort((a, b) => (new Date(b.createdAt) - 
+            new Date(a.createdAt)))
+            setSortedDisplay(sorted)
+            
+        }
+        else setSortedDisplay(display)
+
+    }, [display])
     return (
         <div className="history-page">
             <h2>History</h2>
-            <h4>You have {display?.length} hostorical orders</h4>
+            <h4>You have {sortedDisplay?.length} hostorical orders</h4>
                 <table>
                     <thead>
                         <tr>
@@ -45,7 +60,7 @@ const OrderHistory = () => {
                     </thead>
                     <tbody>
                         {
-                            display?.map((item) => (
+                            sortedDisplay?.map((item) => (
                                 <tr key={item._id}>
                                     <td>{item._id}</td>
                                     <td>{new Date(item.createdAt).toUTCString().substring(0,17)} {new Date(item.createdAt).getHours()}h:{new Date(item.createdAt).getMinutes()}</td>
