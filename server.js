b8ac64f5a59649e7d66a2520d3367f7856aa8623
Mkhululi Cebani <mkhululicebani01@gmail.com>
2022-08-14 21:09:12 +0200

    adding more helmet workers

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ const path = require('path')
 
 //configuring app
 const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
 app.use(helmet.contentSecurityPolicy(
     {
         directives: {
@@ -31,6 +28,26 @@ app.use(helmet.contentSecurityPolicy(
         }
     },
 ))
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
 app.use(fileUpload({
     useTempFiles: true
 }))
