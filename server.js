c530a0f68c592d30090154da31b169e2d6abf3f0
Mkhululi Cebani <mkhululicebani01@gmail.com>
2022-08-14 21:31:15 +0200

    filtering helmet workers2

--- server.js
+++ server.js
@@ -29,14 +29,11 @@ app.use(helmet.contentSecurityPolicy(
     },
 ))
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.expectCt());
 app.use(helmet.frameguard());
 app.use(helmet.hidePoweredBy());
-app.use(helmet.hsts());
+// app.use(helmet.hsts());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
-app.use(helmet.xssFilter());
 
 
 app.use(express.json())
